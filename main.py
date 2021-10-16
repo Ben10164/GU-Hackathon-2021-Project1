06eb6f852c5e0acee0d8c3596aa6b8510573ea2b
Ben <Benpuryear@hotmail.com>
2021-10-16 11:03:01 -0700

    More

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ CHUNK = 1024 * 4 # This is the chunk size
 FORMAT = pyaudio.paInt16 # This is a blank 16-bit binary string
 CHANNELS = 1 # Mono sound
 RATE = 44100 # 44.1 Khz
+
+p = pyaudio.PyAudio()
+
+stream  = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,output=True,frames_per_buffer=CHUNK)
+
+data = stream.read(CHUNK)
