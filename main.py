3b686a7422ef919cbd076e48065499cab2a5804b
Ben <Benpuryear@hotmail.com>
2021-10-16 10:48:59 -0700

    Added basic variables to Main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import pyaudio
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+
+CHUNK = 1024 * 4 # This is the chunk size
+FORMAT = pyaudio.paInt16 # This is a blank 16-bit binary string
+CHANNELS = 1 # Mono sound
+RATE = 44100 # 44.1 Khz
