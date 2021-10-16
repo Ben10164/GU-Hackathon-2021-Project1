5904534872e3f044231588dc05a6627323306dba
Ben <Benpuryear@hotmail.com>
2021-10-16 13:06:50 -0700

    Added Example

--- main.py
+++ main.py
@@ -1,33 +1,102 @@
 import pyaudio
+import os
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from tkinter import TclError
 
 CHUNK = 1024 * 4 # This is the chunk size
 FORMAT = pyaudio.paInt16 # This is a blank 16-bit binary string
 CHANNELS = 1 # Mono sound
 RATE = 44100 # 44.1 Khz
 
+# p = pyaudio.PyAudio()
+
+# stream  = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,output=True,frames_per_buffer=CHUNK)
+
+# fig, ax = plt.subplots()
+
+# x = np.arange(0,2*CHUNK,2)
+# line, = ax.plot(x,np.random.randn(CHUNK))
+# ax.set_ylim(0,255)
+# ax.set_xlim(0,CHUNK)
+
+# while True:
+#     data = stream.read(CHUNK)
+
+#     data_int = np.array(struct.unpack(str(2 * CHUNK)+ 'B', data), dtype='b')[::2] + 127 # This is unpacking the data into an int with length = 2*chunck in bytes
+#     # b is an integer from 0 to 255
+#     # the + 127 will make it so the values larger than 255 will wrap around the graph
+#     line.set_ydata(data_int)
+#     fig.canvas.draw()
+#     fig.canvas.flush_events()
+
+# create matplotlib figure and axes
+fig, ax = plt.subplots(1, figsize=(15, 7))
+
+# pyaudio class instance
 p = pyaudio.PyAudio()
 
-stream  = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,output=True,frames_per_buffer=CHUNK)
+# stream object to get data from microphone
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK
+)
 
-fig, ax = plt.subplots()
+# variable for plotting
+x = np.arange(0, 2 * CHUNK, 2)
 
-x = np.arange(0,2*CHUNK,2)
-line, = ax.plot(x,np.random.randn(CHUNK))
-ax.set_ylim(0,255)
-ax.set_xlim(0,CHUNK)
+# create a line object with random data
+line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
+
+# basic formatting for the axes
+ax.set_title('AUDIO WAVEFORM')
+ax.set_xlabel('samples')
+ax.set_ylabel('volume')
+ax.set_ylim(0, 255)
+ax.set_xlim(0, 2 * CHUNK)
+plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+
+# show the plot
+plt.show(block=False)
+
+print('stream started')
+
+# for measuring frame rate
+frame_count = 0
+start_time = time.time()
 
 while True:
-    data = stream.read(CHUNK)
-
-    data_int = np.array(struct.unpack(str(2 * CHUNK)+ 'B', data), dtype='b')[::2] + 127 # This is unpacking the data into an int with length = 2*chunck in bytes
-    # b is an integer from 0 to 255
-    # the + 127 will make it so the values larger than 255 will wrap around the graph
-    line.set_ydata(data_int)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-
+    
+    # binary data
+    data = stream.read(CHUNK)  
+    
+    # convert data to integers, make np array, then offset it by 127
+    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+    
+    # create np array and offset by 128
+    data_np = np.array(data_int, dtype='b')[::2] + 128
+    
+    line.set_ydata(data_np)
+    
+    # update figure canvas
+    try:
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        frame_count += 1
+        
+    except TclError:
+        
+        # calculate average frame rate
+        frame_rate = frame_count / (time.time() - start_time)
+        
+        print('stream stopped')
+        print('average frame rate = {:.0f} FPS'.format(frame_rate))
+        break
 
 
